6896906f84f71aee8dbc98d013ace67b8311d14b
embersabsalon <embersabsalon@protonmail.com>
2019-11-22 13:53:37 +0800

    Remove firebase configurations from repository

--- celestria/src/App.vue
+++ celestria/src/App.vue
@@ -10,6 +10,7 @@
 <script>
 import Navbar from 'source/components/Navbar';
 import firebase from 'firebase';
+import firebaseConfig from 'source/firebaseConfig'
 
 export default {
   name: 'App',
@@ -18,16 +19,6 @@ export default {
   },
 
   mounted() {
-    var firebaseConfig = {
-      apiKey: "AIzaSyAsQSG4unXwbgA8rcc2uB2mITpiwmmaxUw",
-      authDomain: "celestria-1c12a.firebaseapp.com",
-      databaseURL: "https://celestria-1c12a.firebaseio.com",
-      projectId: "celestria-1c12a",
-      storageBucket: "celestria-1c12a.appspot.com",
-      messagingSenderId: "277710801837",
-      appId: "1:277710801837:web:7159e47794d0218379ef24",
-      measurementId: "G-65SRXP55FR"
-    };
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
   }
